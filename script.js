4a1cbc6dddbcf1d4f252a571c8be83d04d3b9c4c
Begimai8877 <begimai.takeeva.ch@gmail.com>
2022-12-29 12:39:51 +0600

    homeworkJs1

---
+++ script.js
@@ -0,0 +1,50 @@
+// метод перебора  массив foreach. он ничего не возвращает.
+
+// let arr = [1,2,3,3,4,5]
+
+// let newArr = arr.foreach((el) =>{
+//     if(el === 3){
+//         console.log("a")
+//     }else{
+//         console.log(el)
+//     }
+// })
+
+// let arr = [1, 2, 3 , 4, 5, "hello"]
+
+// 3. filter. Перебирает массив. Возвращает новый массив с нужным количеством данных.
+
+// let arr = [1, 2, 3, 4, 5]
+
+// letnewArr = arr.filter((el,idx,a) =>{
+
+// })
+
+// 4. find. перебирает массив. возвращает первый элемент который подходит по условию.find
+// возвращает тот тип данных который у элемента. 
+
+// let a = Math.min(1,2,3,4)
+
+// console.log(a)
+
+// let a = Math.max(1,2,3,4)
+
+// console.log(a)
+
+// Math.min () из нескольких чисел выводит самое минималное let a = Math.min(1,2,3,4)
+// Math.max () из нескольких чисел выводит самое максимальное let a = Math.max(1,2,3,4)
+
+// 5. reduce. Метода который перебирает массив и возвращает нужный тип данных. 
+
+let arr = [1, -4, 7, 12]
+let newArr = arr.filter((el) =>{
+    return el>0
+})
+
+let sum = newArr.reduce((acc,rec) =>{
+    return acc + "" + rec
+}, 0)
+
+console.log (sum)
+
+
